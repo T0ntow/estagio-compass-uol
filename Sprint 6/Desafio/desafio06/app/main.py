2cb0fb9e00585ce8cdda7a414be48118f5e51882
Wellington <wellingtonbs109@gmail.com>
2024-07-28 20:21:54 -0300

    fix: rota do s3

--- Sprint 6/Desafio/desafio06/app/main.py
+++ Sprint 6/Desafio/desafio06/app/main.py
@@ -2,42 +2,30 @@ import boto3
 import os
 from datetime import datetime
 from botocore.config import Config
-import config  # Importe suas configurações do arquivo config.py
 
-def create_s3_session(aws_access_key_id, aws_secret_access_key, aws_session_token, region_name):
-    my_config = Config(
-        region_name=region_name,
-        signature_version='v4',
-        retries={
-            'max_attempts': 10,
-            'mode': 'standard'
-        }
-    )
-    session = boto3.Session(
-        aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key,
-        aws_session_token=aws_session_token,
-        region_name=region_name
-    )
-    return session.client('s3', config=my_config)
+s3_config = Config(
+    region_name='us-east-1'
+)
+s3 = boto3.client('s3', config=s3_config)
 
-# Criar cliente S3 usando configurações do arquivo config.py
-s3_client = create_s3_session(config.aws_access_key_id, config.aws_secret_access_key, config.aws_session_token, config.region_name)
-
-# Função para fazer upload dos arquivos CSV
-def upload_file_to_s3(file_path, bucket_name, storage_layer, data_source, data_format, data_specification):
+def upload_file_to_s3(file_path, bucket_name, storage_layer, data_origin, data_format, data_source):
     file_name = os.path.basename(file_path)
     processing_date = datetime.now().strftime('%Y/%m/%d')
-    s3_key = f"{bucket_name}/{storage_layer}/{data_source}/{data_format}/{data_specification}/{processing_date}/{file_name}"
-
-    # Fazendo upload do arquivo para o S3 usando o cliente s3_client
-    s3_client.upload_file(file_path, bucket_name, s3_key)
-    print(f"Upload {file_name} para {s3_key} concluído.")
+    s3_key = f"{storage_layer}/{data_origin}/{data_format}/{data_source}/{processing_date}/{file_name}"
+    
+    try:
+        s3.upload_file(file_path, bucket_name, s3_key)
+        print(f"Upload {file_name} para {s3_key} concluído.")
+    except Exception as e:
+        print(f"Erro ao fazer upload do arquivo {file_name}: {e}")
 
 # Caminhos dos arquivos CSV
 movies_csv = 'csv_files/movies.csv'
 series_csv = 'csv_files/series.csv'
 
+# Nome do bucket S3
+bucket_name = 'bucketmoviesandseries'
+
 # Fazendo upload dos arquivos CSV de filmes e séries
-upload_file_to_s3(movies_csv, config.bucket_name, 'Raw', 'Movies', 'CSV', 'Movies')
-upload_file_to_s3(series_csv, config.bucket_name, 'Raw', 'Series', 'CSV', 'Series')
+upload_file_to_s3(movies_csv, bucket_name, 'Raw', 'Local', 'CSV', 'Movies')
+upload_file_to_s3(series_csv, bucket_name, 'Raw', 'Local', 'CSV', 'Series')
